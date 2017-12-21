47472b2813ead1f6438435ceae45f4e079f94263
Shaojung <shaojung@gmail.com>
2017-12-21 19:30:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/db2017122101/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.student.db2017122101;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    LocationManager lm;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+}
+
+class MyLocationListener implements LocationListener {
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
